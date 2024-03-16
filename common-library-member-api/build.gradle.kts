cfa53b79cf60c74f8a245dcbd0f6dac9b15cd5da
nostalfinals <yuu8583@proton.me>
2024-03-16 20:57:02 +0800

    build(member): 依赖 utils

---
+++ common-library-member-api/build.gradle.kts
@@ -0,0 +1,3 @@
+dependencies {
+    compileOnly(project(":common-library-utils"))
+}
---
+++ common-library-member-api/src/main/kotlin/ink/pmc/common/member/api/I18n.kt
@@ -0,0 +1,15 @@
+package ink.pmc.common.member.api
+
+import ink.pmc.common.utils.mochaMaroon
+import ink.pmc.common.utils.mochaPeach
+import ink.pmc.common.utils.mochaYellow
+import net.kyori.adventure.text.Component
+
+@Suppress("UNUSED")
+val BAN_DISPLAY = Component.text("封禁").color(mochaMaroon)
+
+@Suppress("UNUSED")
+val WARN_A_DISPLAY = Component.text("A 类警告").color(mochaPeach)
+
+@Suppress("UNUSED")
+val WARN_B_DISPLAY = Component.text("B 类警告").color(mochaYellow)
