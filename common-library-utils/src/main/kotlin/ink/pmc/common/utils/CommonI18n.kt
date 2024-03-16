197dd274fac4041fc802d0342043e328ba821747
nostalfinals <yuu8583@proton.me>
2024-03-16 19:54:48 +0800

    refactor: 替换老的颜色获取

--- common-library-utils/src/main/kotlin/ink/pmc/common/utils/CommonI18n.kt
+++ common-library-utils/src/main/kotlin/ink/pmc/common/utils/CommonI18n.kt
@@ -1,8 +1,6 @@
 package ink.pmc.common.utils
 
-import com.catppuccin.Palette
 import net.kyori.adventure.text.Component
 
 @Suppress("UNUSED")
-val NON_PLAYER = Component.text("该命令仅限玩家使用")
-    .color(Palette.MOCHA.red.toTextColor())
+val NON_PLAYER = Component.text("该命令仅限玩家使用").color(mochaMaroon)
