c7e2a151a6b08b4b102121fd7b51fcf97c1fcb17
nostalfinals <yuu8583@proton.me>
2024-04-12 14:08:50 +0800

    feat: 补充更多的 Comment API

--- common-library-member-api/src/main/kotlin/ink/pmc/common/refactor/member/api/WhitelistStatus.kt
+++ common-library-member-api/src/main/kotlin/ink/pmc/common/refactor/member/api/WhitelistStatus.kt
@@ -7,7 +7,7 @@ import net.kyori.adventure.text.Component
 
 @Suppress("UNUSED")
 enum class WhitelistStatus(val display: Component) {
-
+    
     NON_WHITELISTED(STATUS_NON_WHITELISTED),
     WHITELISTED(STATUS_WHITELISTED),
     WHITELISTED_BEFORE(STATUS_WHITELISTED_BEFORE)
--- common-library-member-api/src/main/kotlin/ink/pmc/common/refactor/member/api/comment/CommentRepository.kt
+++ common-library-member-api/src/main/kotlin/ink/pmc/common/refactor/member/api/comment/CommentRepository.kt
@@ -5,9 +5,15 @@ interface CommentRepository {
 
     fun list(): Collection<Comment>
 
-    fun comment(blockL: CommentDsl.() -> Unit): Comment
+    fun comment(creator: Long, content: String): Comment
+
+    operator fun set(creator: Long, content: String)
 
     fun modify(id: Long, new: String): Comment
+
+    fun lookup(id: Long): Comment?
+
+    operator fun get(id: Long): Comment?
 
     fun uncomment(id: Long)
 
