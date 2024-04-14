6de1a008fdc5bcf88a10fcf08c34a17f496603ec
nostalfinals <yuu8583@proton.me>
2024-04-15 01:51:19 +0800

    fix: 修复 LittleSkin 验证问题

--- common-library-member-api/src/main/kotlin/ink/pmc/common/member/api/fetcher/LittleSkinProfileFetcher.kt
+++ common-library-member-api/src/main/kotlin/ink/pmc/common/member/api/fetcher/LittleSkinProfileFetcher.kt
@@ -1,5 +1,7 @@
 package ink.pmc.common.member.api.fetcher
 
+import com.google.gson.JsonParser
+import ink.pmc.common.utils.player.shortUUIDToLong
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaType
@@ -13,21 +15,24 @@ private const val littleSkinApi = "https://littleskin.cn/api/yggdrasil/"
 @Suppress("UNUSED")
 object LittleSkinProfileFetcher : AbstractProfileFetcher() {
 
-    override suspend fun fetch(name: String): UUID? {
-        return withContext(Dispatchers.IO) {
-            try {
-                val reqBodyJson = "[\"${name.lowercase()}\"]"
-                val request = Request.Builder()
-                    .url(littleSkinApi + "api/profiles/minecraft/")
-                    .header("Content-Type", "application/json; charset=utf-8")
-                    .commonPost(reqBodyJson.toRequestBody("application/json".toMediaType()))
-                    .build()
+    override suspend fun fetch(name: String): UUID? = withContext(Dispatchers.IO) {
+        val reqBodyJson = "[\"${name.lowercase()}\"]"
+        val request = Request.Builder()
+            .url(littleSkinApi + "api/profiles/minecraft/")
+            .header("Content-Type", "application/json; charset=utf-8")
+            .commonPost(reqBodyJson.toRequestBody("application/json".toMediaType()))
+            .build()
 
-                return@withContext yggGet(request)
-            } catch (e: Exception) {
-                null
-            }
-        }
+        val call = httpClient.newCall(request)
+
+        val response = call.execute()
+        val body = response.body
+        val jsonArray = JsonParser.parseString(body.string()).asJsonArray ?: return@withContext null
+        val element = jsonArray.get(0).asJsonObject.get("id") ?: return@withContext null
+        val shortUUID = element.asString ?: return@withContext null
+
+        val uuid = shortUUID.shortUUIDToLong
+        UUID.fromString(uuid)
     }
 
 }
