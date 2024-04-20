70f9806961be0338349578b3a2718eba2881aab2
nostalfinals <yuu8583@proton.me>
2024-04-20 23:15:12 +0800

    feat: 基岩版消息映射

--- common-library-utils/src/main/kotlin/ink/pmc/common/utils/bedrock/PaperPlayerExtensions.kt
+++ common-library-utils/src/main/kotlin/ink/pmc/common/utils/bedrock/PaperPlayerExtensions.kt
@@ -1,7 +1,22 @@
 package ink.pmc.common.utils.bedrock
 
+import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 
 @Suppress("UNUSED")
 val Player.isBedrock: Boolean
     get() = isBedrockSession(this.uniqueId)
+
+@Suppress("UNUSED")
+fun Player.fallback(original: Component, bedrock: Component): Component {
+    if (this.isBedrock) {
+        return bedrock
+    }
+
+    return original
+}
+
+@Suppress("UNUSED")
+fun Player.sendMessage(component: Component, fallback: Component) {
+    this.sendMessage(this.fallback(component, fallback))
+}
--- common-library-utils/src/main/kotlin/ink/pmc/common/utils/bedrock/VelocityPlayerExtensions.kt
+++ common-library-utils/src/main/kotlin/ink/pmc/common/utils/bedrock/VelocityPlayerExtensions.kt
@@ -1,7 +1,22 @@
 package ink.pmc.common.utils.bedrock
 
 import com.velocitypowered.api.proxy.Player
+import net.kyori.adventure.text.Component
 
 @Suppress("UNUSED")
 val Player.isBedrock: Boolean
     get() = isBedrockSession(this.uniqueId)
+
+@Suppress("UNUSED")
+fun Player.fallback(original: Component, bedrock: Component): Component {
+    if (this.isBedrock) {
+        return bedrock
+    }
+
+    return original
+}
+
+@Suppress("UNUSED")
+fun Player.sendMessage(component: Component, fallback: Component) {
+    this.sendMessage(this.fallback(component, fallback))
+}
