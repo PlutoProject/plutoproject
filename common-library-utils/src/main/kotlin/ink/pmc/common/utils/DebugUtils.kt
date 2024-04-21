cad9be02e91223ec38e0e93ec2d0833c9767570e
nostalfinals <yuu8583@proton.me>
2024-04-21 18:31:57 +0800

    refactor: 去除调试模式的输出

--- common-library-utils/src/main/kotlin/ink/pmc/common/utils/DebugUtils.kt
+++ common-library-utils/src/main/kotlin/ink/pmc/common/utils/DebugUtils.kt
@@ -1,18 +1,6 @@
 package ink.pmc.common.utils
 
-import java.util.logging.Logger
-
-private val logger = Logger.getLogger("DEBUG MODE CHECK")
-
 @Suppress("UNUSED")
 fun isInDebugMode(): Boolean {
-    if (System.getProperty("PLUTO_DEBUG_MODE") != null) {
-        logger.warning("A plugin triggered the debug mode check method.")
-        logger.warning("The server is running debug mode in Gradle test plugin.")
-        logger.warning("Some logic will be disabled.")
-
-        return true
-    }
-
-    return false
+    return System.getProperty("PLUTO_DEBUG_MODE") != null
 }
