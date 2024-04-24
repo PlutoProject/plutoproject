0cb0d982476acb42078f91d45cfb65fd4b3e9edf
nostalfinals <yuu8583@proton.me>
2024-04-24 15:46:28 +0800

    feat: 更新协议版本号

--- bedrock-adaptive/velocity/src/main/kotlin/ink/pmc/bedrockadaptive/VelocityPlugin.kt
+++ bedrock-adaptive/velocity/src/main/kotlin/ink/pmc/bedrockadaptive/VelocityPlugin.kt
@@ -19,7 +19,7 @@ import java.util.logging.Logger
 
 lateinit var serverLogger: Logger
 lateinit var pluginContainer: PluginContainer
-val protocolVersion = ProtocolVersion.MINECRAFT_1_20_3
+val protocolVersion = ProtocolVersion.MINECRAFT_1_20_5
 
 @Suppress("UNUSED", "UNUSED_PARAMETER")
 class VelocityPlugin @Inject constructor(suspendingPluginContainer: SuspendingPluginContainer) {
