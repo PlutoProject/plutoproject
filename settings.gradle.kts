5656b7bb090bc371cdcaadaeb2132697f1dc02ba
nostalfinals <yuu8583@proton.me>
2024-06-09 00:50:00 +0800

    chore: 改名！！

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,22 +10,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "common"
-
-// API 部分
-/*include("common-library-member-api")
-include("common-library-misc-api")
-include("common-library-rpc-api")
-include("common-library-exchange-api")*/
-
-// 功能 & 实现部分
-/*include("common-member")
-include("common-misc")
-include("common-utils")
-include("common-hypervisor")
-include("common-rpc")
-include("common-bedrock-adaptive")
-include("common-exchange")*/
+rootProject.name = "plutoproject"
 
 include("bedrock-adaptive")
 include("bedrock-adaptive:velocity")
