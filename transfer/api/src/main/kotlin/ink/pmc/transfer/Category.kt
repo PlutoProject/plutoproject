23dcf5a4d14cafe1f5200253a18d17c9b40c5c83
nostalfinals <yuu8583@proton.me>
2024-06-16 07:42:46 +0800

    feat: Category 添加图标

--- transfer/api/src/main/kotlin/ink/pmc/transfer/Category.kt
+++ transfer/api/src/main/kotlin/ink/pmc/transfer/Category.kt
@@ -1,11 +1,13 @@
 package ink.pmc.transfer
 
+import ink.pmc.utils.multiplaform.item.KeyedMaterial
 import net.kyori.adventure.text.Component
 
 @Suppress("UNUSED")
 interface Category {
 
     val id: String
+    val icon: KeyedMaterial
     val name: Component
     val description: Component
     val totalPlayers: Int
