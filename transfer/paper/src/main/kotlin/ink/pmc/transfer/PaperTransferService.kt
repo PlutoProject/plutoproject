2ab97a3120bba1cc25204df5c6dc4ee022769a30
nostalfinals <yuu8583@proton.me>
2024-06-17 23:29:00 +0800

    feat: 提供通用实现

--- transfer/paper/src/main/kotlin/ink/pmc/transfer/PaperTransferService.kt
+++ transfer/paper/src/main/kotlin/ink/pmc/transfer/PaperTransferService.kt
@@ -1,15 +1,6 @@
 package ink.pmc.transfer
 
-import ink.pmc.utils.multiplaform.player.PlayerWrapper
-
 class PaperTransferService : BaseTransferServiceImpl() {
-
-    override val playerCount: Int
-        get() = TODO("Not yet implemented")
-
-    override suspend fun transferPlayer(player: PlayerWrapper<*>, id: String) {
-        player.switchServer(id)
-    }
 
     override fun close() {
         TODO("Not yet implemented")
--- transfer/velocity/src/main/kotlin/ink/pmc/transfer/ProxyTransferService.kt
+++ transfer/velocity/src/main/kotlin/ink/pmc/transfer/ProxyTransferService.kt
@@ -2,19 +2,12 @@ package ink.pmc.transfer
 
 import ink.pmc.transfer.api.ConditionManager
 import ink.pmc.transfer.proto.TransferRpc
-import ink.pmc.utils.multiplaform.player.PlayerWrapper
 import ink.pmc.utils.platform.proxy
 
 class ProxyTransferService : AbstractProxyTransferService() {
 
     override val protocol: TransferRpc = TransferRpc(proxy, this)
     override val conditionManager: ConditionManager = ConditionManagerImpl(this)
-    override var playerCount: Int = 0
-
-    override suspend fun transferPlayer(player: PlayerWrapper<*>, id: String) {
-        val destination = getDestination(id) ?: throw IllegalStateException("Destination named as $id not existed")
-        destination.transfer(player)
-    }
 
     override fun close() {
         protocol.close()
