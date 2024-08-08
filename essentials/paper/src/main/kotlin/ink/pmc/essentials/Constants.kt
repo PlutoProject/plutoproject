8ccf6cf2ed38f4708867b7db9eba4d1500b234e4
nostalfinals <yuu8583@proton.me>
2024-08-08 18:43:36 +0800

    feat: homes 指令

--- essentials/paper/src/main/kotlin/ink/pmc/essentials/Constants.kt
+++ essentials/paper/src/main/kotlin/ink/pmc/essentials/Constants.kt
@@ -11,3 +11,5 @@ const val LECTERN_PROTECT_BYPASS = "essentials.lectern.bypass"
 const val RANDOM_TELEPORT_COST_BYPASS = "essentials.rtp.bypass"
 
 const val RANDOM_TELEPORT_SPECIFIC = "essentials.rtp.specific"
+
+const val HOMES_OTHER = "essentials.home.other"
--- essentials/paper/src/main/kotlin/ink/pmc/essentials/commands/home/HomesCommand.kt
+++ essentials/paper/src/main/kotlin/ink/pmc/essentials/commands/home/HomesCommand.kt
@@ -2,22 +2,44 @@ package ink.pmc.essentials.commands.home
 
 import cafe.adriel.voyager.navigator.Navigator
 import ink.pmc.essentials.Cm
+import ink.pmc.essentials.HOMES_OTHER
 import ink.pmc.essentials.commands.checkPlayer
 import ink.pmc.essentials.screens.HomeViewerScreen
 import ink.pmc.interactive.inventory.canvas.inv
 import ink.pmc.utils.annotation.Command
+import ink.pmc.utils.chat.NO_PERMISSON
+import ink.pmc.utils.command.suggestion.PaperPrivilegedSuggestion
 import ink.pmc.utils.dsl.cloud.invoke
 import ink.pmc.utils.dsl.cloud.sender
-import org.incendo.cloud.bukkit.parser.PlayerParser
+import org.bukkit.OfflinePlayer
+import org.incendo.cloud.bukkit.parser.OfflinePlayerParser
+import org.incendo.cloud.bukkit.parser.WorldParser
+import kotlin.jvm.optionals.getOrNull
 
 @Command("homes")
 @Suppress("UNUSED")
 fun Cm.homes(aliases: Array<String>) {
     this("homes", *aliases) {
         permission("essentials.homes")
-        optional("player", PlayerParser.playerParser())
+        optional(
+            "player",
+            OfflinePlayerParser.offlinePlayerParser(),
+            PaperPrivilegedSuggestion.of(WorldParser(), HOMES_OTHER)
+        )
         handler {
             checkPlayer(sender.sender) {
+                val argPlayer = optional<OfflinePlayer>("player").getOrNull()
+
+                if (argPlayer != null) {
+                    if (!hasPermission(HOMES_OTHER)) {
+                        sendMessage(NO_PERMISSON)
+                        return@checkPlayer
+                    }
+                    inv {
+                        Navigator(HomeViewerScreen(this, argPlayer))
+                    }
+                }
+
                 inv {
                     Navigator(HomeViewerScreen(this, this))
                 }
