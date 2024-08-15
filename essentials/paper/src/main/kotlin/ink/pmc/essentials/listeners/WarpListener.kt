61103c9bbcd87b3f68cc5e050b48f8a2fa827088
nostalfinals <yuu8583@proton.me>
2024-08-16 07:37:56 +0800

    feat: 自动为玩家设置首选的主城

--- essentials/paper/src/main/kotlin/ink/pmc/essentials/listeners/WarpListener.kt
+++ essentials/paper/src/main/kotlin/ink/pmc/essentials/listeners/WarpListener.kt
@@ -4,6 +4,7 @@ import ink.pmc.essentials.api.warp.WarpManager
 import ink.pmc.utils.concurrent.submitAsync
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.server.ServerLoadEvent
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -18,6 +19,13 @@ object WarpListener : Listener, KoinComponent {
         submitAsync {
             manager.list()
         }
+    }
+
+    @EventHandler
+    suspend fun PlayerJoinEvent.e() {
+        if (manager.getPreferredSpawn(player) != null) return
+        val default = manager.getDefaultSpawn() ?: return
+        manager.setPreferredSpawn(player, default)
     }
 
 }
