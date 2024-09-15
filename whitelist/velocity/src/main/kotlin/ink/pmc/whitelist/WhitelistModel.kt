bf133c73b3da4c2110bf914d7c0528f82b20460b
nostalfinals <yuu8583@proton.me>
2024-09-15 18:42:03 +0800

    fix: 序列化

--- whitelist/velocity/src/main/kotlin/ink/pmc/whitelist/WhitelistModel.kt
+++ whitelist/velocity/src/main/kotlin/ink/pmc/whitelist/WhitelistModel.kt
@@ -3,12 +3,14 @@ package ink.pmc.whitelist
 import ink.pmc.utils.currentUnixTimestamp
 import ink.pmc.whitelist.profile.ProfileFetcher
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import java.util.*
 
 fun createWhitelistModel(uuid: UUID, name: String, fetcher: ProfileFetcher): WhitelistModel {
     return WhitelistModel(uuid.toString(), name, currentUnixTimestamp, fetcher.id)
 }
 
+@Serializable
 data class WhitelistModel(
     @SerialName("_id") val id: String,
     var rawName: String,
