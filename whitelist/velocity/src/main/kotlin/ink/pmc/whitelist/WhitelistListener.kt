9dd1ae7a4b5fc1e5d71324387ee497ffc2f80018
nostalfinals <yuu8583@proton.me>
2024-09-29 12:42:17 +0800

    fix: 修复白名单

--- whitelist/velocity/src/main/kotlin/ink/pmc/whitelist/WhitelistListener.kt
+++ whitelist/velocity/src/main/kotlin/ink/pmc/whitelist/WhitelistListener.kt
@@ -1,13 +1,10 @@
 package ink.pmc.whitelist
 
-import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.connection.PostLoginEvent
-import com.velocitypowered.api.event.connection.PreLoginEvent
 import ink.pmc.advkt.component.component
 import ink.pmc.advkt.component.newline
 import ink.pmc.advkt.component.text
-import ink.pmc.utils.concurrent.submitAsync
 import ink.pmc.utils.visual.mochaMaroon
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -17,27 +14,23 @@ object WhitelistListener : KoinComponent {
 
     private val repo by inject<WhitelistRepository>()
 
-    // 最后执行，等待 Floodgate 或其他插件的 Profile 替换逻辑
-    @Subscribe(order = PostOrder.LAST)
-    suspend fun PreLoginEvent.e() {
-        uniqueId?.let {
-            if (repo.hasById(it)) return
-        }
-        result = PreLoginEvent.PreLoginComponentResult.denied(component {
-            text("此 ID 尚未获得白名单") with mochaMaroon
-            newline()
-            text("若已通过审核，请联系当日的审核员添加") with mochaMaroon
-        })
-    }
-
     @Subscribe
-    fun PostLoginEvent.e() {
-        submitAsync {
-            val model = repo.findById(player.uniqueId) ?: return@submitAsync
-            if (model.rawName == player.username) return@submitAsync
-            model.rawName = player.username
-            repo.saveOrUpdate(model)
+    suspend fun PostLoginEvent.e() {
+        run {
+            player.uniqueId.let {
+                if (repo.hasById(it)) return@run
+            }
+            player.disconnect(component {
+                text("此 ID 尚未获得白名单") with mochaMaroon
+                newline()
+                text("若已通过审核，请联系当日的审核员添加") with mochaMaroon
+            })
         }
+
+        val model = repo.findById(player.uniqueId) ?: return
+        if (model.rawName == player.username) return
+        model.rawName = player.username
+        repo.saveOrUpdate(model)
     }
 
 }
