f85d4b1c0e8549dea73ec69cae9d41318280b499
nostalfinals <yuu8583@proton.me>
2024-10-21 16:14:45 +0800

    1

--- hypervisor/api/src/main/kotlin/ink/pmc/hypervisor/DynamicScheduling.kt
+++ hypervisor/api/src/main/kotlin/ink/pmc/hypervisor/DynamicScheduling.kt
@@ -1,8 +1,11 @@
 package ink.pmc.hypervisor
 
+import ink.pmc.utils.inject.inlinedGet
 import org.bukkit.entity.Player
 
 interface DynamicScheduling {
+    companion object : DynamicScheduling by inlinedGet()
+
     val enabled: Boolean
 
     fun setDynamicViewDistanceState(player: Player, state: Boolean)
