5baa68b30e49a13eb2c64d60938e986712cd97be
nostalfinals <yuu8583@proton.me>
2024-10-21 19:17:54 +0800

    feat: 完善 Config

--- hypervisor/paper/src/main/kotlin/ink/pmc/hypervisor/config/DynamicScheduling.kt
+++ hypervisor/paper/src/main/kotlin/ink/pmc/hypervisor/config/DynamicScheduling.kt
@@ -1,5 +1,33 @@
 package ink.pmc.hypervisor.config
 
+import org.bukkit.entity.SpawnCategory
+
+typealias CurveSampler = Map<Int, Int>
+typealias SpawnStrategy = Map<SpawnCategory, CurveSampler>
+
 data class DynamicScheduling(
-    val enabled: Boolean
+    val enabled: Boolean,
+    val cyclePeriod: Long,
+    val viewDistance: ViewDistance,
+    val simulateDistance: SimulateDistance,
+    val spawnLimits: SpawnSettings,
+    val ticksPerSpawn: SpawnSettings,
+)
+
+data class ViewDistance(
+    val enabled: Boolean,
+    val virtualHosts: List<String>,
+    val maximumPing: Double,
+    val sampler: CurveSampler
+)
+
+data class SimulateDistance(
+    val enabled: Boolean,
+    val sampler: CurveSampler,
+)
+
+data class SpawnSettings(
+    val enabled: Boolean,
+    val default: SpawnStrategy,
+    val world: Map<String, SpawnStrategy>
 )
