df3b90007e17879ddb96c11e08d2498897dd5a55
nostalfinals <yuu8583@proton.me>
2024-10-22 22:43:44 +0800

    内联

--- utils/api/src/main/kotlin/ink/pmc/utils/data/Multimap.kt
+++ utils/api/src/main/kotlin/ink/pmc/utils/data/Multimap.kt
@@ -4,10 +4,12 @@ import com.google.common.collect.ArrayListMultimap
 import com.google.common.collect.Multimap
 import com.google.common.collect.Multimaps
 
-fun <K, V> listMultimapOf(): Multimap<K, V> {
+@Suppress("NOTHING_TO_INLINE")
+inline fun <K, V> listMultimapOf(): Multimap<K, V> {
     return ArrayListMultimap.create()
 }
 
-fun <K, V> concurrentListMultimapOf(): Multimap<K, V> {
+@Suppress("NOTHING_TO_INLINE")
+inline fun <K, V> concurrentListMultimapOf(): Multimap<K, V> {
     return Multimaps.synchronizedMultimap(listMultimapOf())
 }
--- utils/api/src/main/kotlin/ink/pmc/utils/data/MultimapExts.kt
+++ utils/api/src/main/kotlin/ink/pmc/utils/data/MultimapExts.kt
@@ -2,10 +2,12 @@ package ink.pmc.utils.data
 
 import com.google.common.collect.Multimap
 
-operator fun <K, V> Multimap<K, V>.set(key: K, value: V) {
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <K, V> Multimap<K, V>.set(key: K, value: V) {
     put(key, value)
 }
 
-operator fun <K, V> Multimap<K, V>.set(key: K, value: Iterable<V>) {
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <K, V> Multimap<K, V>.set(key: K, value: Iterable<V>) {
     putAll(key, value)
 }
