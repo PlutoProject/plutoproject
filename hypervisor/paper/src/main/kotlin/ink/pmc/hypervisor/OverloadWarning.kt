f561a52e2e4af0d83b63759801b1211e7b885d00
nostalfinals <yuu8583@proton.me>
2024-10-23 01:14:26 +0800

    优化提示文本

--- hypervisor/paper/src/main/kotlin/ink/pmc/hypervisor/OverloadWarning.kt
+++ hypervisor/paper/src/main/kotlin/ink/pmc/hypervisor/OverloadWarning.kt
@@ -27,15 +27,19 @@ object OverloadWarning : KoinComponent {
                 val millsPerTick = StatisticProvider.getMillsPerTick(MeasuringTime.SECONDS_10)
                 if (millsPerTick != null && millsPerTick > 50) {
                     paper.broadcast(component {
+                        newline()
                         text("⚠ ") with mochaYellow
                         text("温馨提示 ") with mochaText
                         text("»") with mochaSubtext0
                         newline()
                         text("服务器目前处于严重过载状态，可能会出现生物停滞、不刷怪等现象") with mochaYellow
                         newline()
-                        text("请关闭正在运行的机器、暂缓跑图；涉及到 TNT 的机器，需尽快关闭以避免损坏") with mochaYellow
+                        text("请关闭正在运行的机器、暂缓跑图") with mochaYellow
+                        newline()
+                        text("涉及到 TNT 的机器，需尽快关闭以避免损坏") with mochaYellow
                         newline()
                         text("稳定流畅的游戏体验需大家一同维护，感谢配合") with mochaYellow
+                        newline()
                     })
                 }
                 delay(config.overloadWarning.cyclePeriod)
