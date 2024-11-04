d87edeaa1d7bc0e109916f43f03d5f5540e80031
nostalfinals <yuu8583@proton.me>
2024-11-04 20:17:01 +0800

    style: suppress

--- framework/api/src/main/kotlin/ink/pmc/framework/utils/command/MethodArgumentParsers.kt
+++ framework/api/src/main/kotlin/ink/pmc/framework/utils/command/MethodArgumentParsers.kt
@@ -5,13 +5,13 @@ import org.incendo.cloud.CommandManager
 import org.incendo.cloud.annotations.parser.MethodArgumentParser
 import org.incendo.cloud.parser.ArgumentParser
 
-@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
+@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE",  "UnusedReceiverParameter")
 inline fun <C, T> CommandManager<C>.getKotlinMethodArgumentParser(): Class<ArgumentParser<C, T>> {
     return (findClass("org.incendo.cloud.kotlin.coroutines.annotations.KotlinMethodArgumentParser")
         ?: error("Env error: cannot find KotlinMethodArgumentParser class")) as Class<ArgumentParser<C, T>>
 }
 
-@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
+@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE",  "UnusedReceiverParameter")
 inline fun <C, T> CommandManager<C>.getMethodArgumentParser(): Class<ArgumentParser<C, T>> {
     return MethodArgumentParser::class.java as Class<ArgumentParser<C, T>>
 }
