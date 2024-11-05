4d80dd89f9802029bb3f2b298c55fcfe1bf0ae58
nostalfinals <yuu8583@proton.me>
2024-11-05 22:27:01 +0800

    refactor(essentials): 移除 WarpMenu 中未使用的导入和空实现
    - 删除了未使用的导入语句，包括 Item、Modifier、clickable、Menu、Component 和 Material
    - 移除了 WarpMenu 类中 Content 函数的空实现

--- essentials/paper/src/main/kotlin/ink/pmc/essentials/screens/warp/WarpMenu.kt
+++ essentials/paper/src/main/kotlin/ink/pmc/essentials/screens/warp/WarpMenu.kt
@@ -4,12 +4,6 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import ink.pmc.framework.interactive.LocalPlayer
-import ink.pmc.framework.interactive.inventory.Item
-import ink.pmc.framework.interactive.inventory.Modifier
-import ink.pmc.framework.interactive.inventory.click.clickable
-import ink.pmc.framework.interactive.inventory.layout.Menu
-import net.kyori.adventure.text.Component
-import org.bukkit.Material
 
 class WarpMenu : Screen {
     override val key: ScreenKey = "essentials_warp_menu"
@@ -17,12 +11,5 @@ class WarpMenu : Screen {
     @Composable
     override fun Content() {
         val player = LocalPlayer.current
-        Menu(title = Component.text("地标")) {
-            repeat(21) {
-                Item(material = Material.PAPER, modifier = Modifier.clickable {
-                    player.sendMessage("$it")
-                })
-            }
-        }
     }
 }
