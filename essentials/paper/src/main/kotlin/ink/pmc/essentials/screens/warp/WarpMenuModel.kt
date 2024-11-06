278883c42e5c8c39addf69c6648f5b2bc7027d91
nostalfinals <yuu8583@proton.me>
2024-11-06 19:56:29 +0800

    feat(warp): 实现传送点菜单模型
    - 添加 filter 属性以支持筛选功能
    - 新增 isLoading、pageCount、currentPage 和 content 属性以支持分页加载
    - 实现 nextFilter 和 previousFilter 方法以切换筛选条件
    - 添加 loadPage 方法以加载指定页面的传送点数据
    - 修改 Filter枚举，将 STARRED 改为 COLLECTED，以更准确地表示已收藏的传送点

--- essentials/paper/src/main/kotlin/ink/pmc/essentials/screens/warp/WarpMenuModel.kt
+++ essentials/paper/src/main/kotlin/ink/pmc/essentials/screens/warp/WarpMenuModel.kt
@@ -1,20 +1,44 @@
 package ink.pmc.essentials.screens.warp
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import cafe.adriel.voyager.core.model.ScreenModel
+import ink.pmc.essentials.config.Warp
 import ink.pmc.essentials.screens.warp.WarpMenuModel.Filter.*
 
+private const val PAGE_SIZE = 36
+
 class WarpMenuModel : ScreenModel {
+    var filter by mutableStateOf(ALL)
+    var isLoading by mutableStateOf(true)
+    var pageCount by mutableStateOf(-1)
+    var currentPage by mutableStateOf(-1)
+    var content = mutableStateListOf<Warp>()
+
     enum class Filter {
-        ALL, STARRED, MACHINE, ARCHITECTURE, TOWN
+        ALL, COLLECTED, MACHINE, ARCHITECTURE, TOWN
+    }
+
+    suspend fun loadPage(page: Int) {
+        isLoading = true
+        pageCount = -1
+        currentPage = -1
+        content.clear()
+        when (filter) {
+            ALL -> TODO()
+            COLLECTED -> TODO()
+            MACHINE -> TODO()
+            ARCHITECTURE -> TODO()
+            TOWN -> TODO()
+        }
     }
 
     fun nextFilter() {
         filter = when (filter) {
-            ALL -> STARRED
-            STARRED -> MACHINE
+            ALL -> COLLECTED
+            COLLECTED -> MACHINE
             MACHINE -> ARCHITECTURE
             ARCHITECTURE -> TOWN
             TOWN -> ALL
@@ -24,12 +48,10 @@ class WarpMenuModel : ScreenModel {
     fun previousFilter() {
         filter = when (filter) {
             ALL -> TOWN
-            STARRED -> ALL
-            MACHINE -> STARRED
+            COLLECTED -> ALL
+            MACHINE -> COLLECTED
             ARCHITECTURE -> MACHINE
             TOWN -> ARCHITECTURE
         }
     }
-
-    var filter by mutableStateOf(ALL)
 }
