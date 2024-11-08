2c23fb8720f3d3299d457f9dc75ab88c1e788807
nostalfinals <yuu8583@proton.me>
2024-11-08 19:53:21 +0800

    feat(bridge): 实现远程后端玩家操作功能
    - 添加 BridgeLocation.toInfo() 扩展函数，用于将 BridgeLocation 转换为 LocationInfo
    - 修改 ProxyRemoteBackendPlayer 类，增加对远程后端玩家的各种操作支持
    - 实现了包括传送、发送消息、显示标题、播放音效和执行命令在内的多种玩家操作
    - 增加了对玩家和服务器在线状态的检查，提高了代码的健壮性

--- framework/shared/src/main/kotlin/ink/pmc/framework/bridge/BridgeLocationImpl.kt
+++ framework/shared/src/main/kotlin/ink/pmc/framework/bridge/BridgeLocationImpl.kt
@@ -1,12 +1,26 @@
 package ink.pmc.framework.bridge
 
 import ink.pmc.framework.bridge.proto.BridgeRpcOuterClass.LocationInfo
+import ink.pmc.framework.bridge.proto.locationInfo
 import ink.pmc.framework.bridge.server.BridgeServer
 import ink.pmc.framework.bridge.world.BridgeLocation
 import ink.pmc.framework.bridge.world.BridgeWorld
 
 fun LocationInfo.toImpl(server: BridgeServer, world: BridgeWorld): BridgeLocation {
     return BridgeLocationImpl(server, world, x, y, z, yaw, pitch)
+}
+
+fun BridgeLocation.toInfo(): LocationInfo {
+    val loc = this
+    return locationInfo {
+        server = loc.server.id
+        world = loc.world.name
+        x = loc.x
+        y = loc.y
+        z = loc.z
+        yaw = loc.yaw
+        pitch = loc.pitch
+    }
 }
 
 data class BridgeLocationImpl(
--- framework/velocity/src/main/kotlin/ink/pmc/framework/bridge/player/ProxyRemoteBackendPlayer.kt
+++ framework/velocity/src/main/kotlin/ink/pmc/framework/bridge/player/ProxyRemoteBackendPlayer.kt
@@ -7,12 +7,14 @@ import ink.pmc.advkt.title.ComponentTitleKt
 import ink.pmc.framework.bridge.BridgeLocationImpl
 import ink.pmc.framework.bridge.BridgeRpc
 import ink.pmc.framework.bridge.InternalPlayer
+import ink.pmc.framework.bridge.proto.BridgeRpcOuterClass.PlayerOperationResult
 import ink.pmc.framework.bridge.proto.BridgeRpcOuterClass.PlayerOperationResult.ContentCase.*
 import ink.pmc.framework.bridge.proto.playerOperation
 import ink.pmc.framework.bridge.server.BridgeGroup
 import ink.pmc.framework.bridge.server.BridgeServer
 import ink.pmc.framework.bridge.server.ServerType
 import ink.pmc.framework.bridge.server.localServer
+import ink.pmc.framework.bridge.toInfo
 import ink.pmc.framework.bridge.world.BridgeLocation
 import ink.pmc.framework.bridge.world.BridgeWorld
 import ink.pmc.framework.utils.concurrent.submitAsync
@@ -34,8 +36,10 @@ class ProxyRemoteBackendPlayer(
     override val location: Deferred<BridgeLocation>
         get() = submitAsync<BridgeLocation> {
             check(server.isOnline) { "Server offline" }
+            check(isOnline) { "Player offline" }
             val result = BridgeRpc.operatePlayer(playerOperation {
                 id = UUID.randomUUID().toString()
+                playerUuid = uniqueId.toString()
                 remoteBackend = true
                 infoLookup = empty
             })
@@ -51,48 +55,85 @@ class ProxyRemoteBackendPlayer(
                 CONTENT_NOT_SET -> error("Unexpected")
             }
         }
-    override var isOnline: Boolean = true
+    override var isOnline: Boolean
+        set(_) {}
+        get() = actual.isOnlineMode
 
     private val local = localServer.getPlayer(uniqueId)!!
 
+    private fun checkNoReturnResult(result: PlayerOperationResult, operation: String) {
+        when (result.contentCase!!) {
+            OK -> {}
+            PLAYER_OFFLINE -> error("Player offline")
+            SERVER_OFFLINE -> error("Server offline")
+            WORLD_NOT_FOUND -> error("Unexpected")
+            UNSUPPORTED -> error("Unsupported")
+            TIMEOUT -> error("Timeout while $operation")
+            CONTENT_NOT_SET -> error("Unexpected")
+        }
+    }
+
     override suspend fun teleport(location: BridgeLocation) {
-        TODO("Not yet implemented")
+        check(server.isOnline) { "Server offline" }
+        check(isOnline) { "Player offline" }
+        val result = BridgeRpc.operatePlayer(playerOperation {
+            id = UUID.randomUUID().toString()
+            playerUuid = uniqueId.toString()
+            remoteBackend = true
+            teleport = location.toInfo()
+        })
+        checkNoReturnResult(result, "teleporting $name")
     }
 
     override suspend fun teleport(world: BridgeWorld) {
-        TODO("Not yet implemented")
+        teleport(world.spawnPoint)
     }
 
     override suspend fun sendMessage(message: String) {
+        check(isOnline) { "Player offline" }
         local.sendMessage(message)
     }
 
     override suspend fun sendMessage(message: Component) {
+        check(isOnline) { "Player offline" }
         local.sendMessage(message)
     }
 
     override suspend fun sendMessage(message: RootComponentKt.() -> Unit) {
+        check(isOnline) { "Player offline" }
         local.sendMessage(message)
     }
 
     override suspend fun showTitle(title: Title) {
+        check(isOnline) { "Player offline" }
         local.showTitle(title)
     }
 
     override suspend fun showTitle(title: ComponentTitleKt.() -> Unit) {
+        check(isOnline) { "Player offline" }
         local.showTitle(title)
     }
 
     override suspend fun playSound(sound: Sound) {
+        check(isOnline) { "Player offline" }
         local.playSound(sound)
     }
 
     override suspend fun playSound(sound: SoundKt.() -> Unit) {
+        check(isOnline) { "Player offline" }
         local.playSound(sound)
     }
 
     override suspend fun performCommand(command: String) {
-
+        check(server.isOnline) { "Server offline" }
+        check(isOnline) { "Player offline" }
+        val result = BridgeRpc.operatePlayer(playerOperation {
+            id = UUID.randomUUID().toString()
+            playerUuid = uniqueId.toString()
+            remoteBackend = true
+            performCommand = command
+        })
+        checkNoReturnResult(result, "performing command ($command) on $name")
     }
 
     override fun convertElement(type: ServerType): BridgePlayer? {
