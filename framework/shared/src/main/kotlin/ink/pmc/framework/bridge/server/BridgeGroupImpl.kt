6e00a5ef7542737dec94fc9155396baf9f0e824b
nostalfinals <yuu8583@proton.me>
2024-11-09 21:17:37 +0800

    refactor(framework): 优化世界信息获取代码
    - 简化了 RemoteBackendWorld 初始化时的参数传递
    - 使用单行代码替代了多行参数，提高了代码可读性

--- framework/shared/src/main/kotlin/ink/pmc/framework/bridge/server/BridgeGroupImpl.kt
+++ framework/shared/src/main/kotlin/ink/pmc/framework/bridge/server/BridgeGroupImpl.kt
@@ -3,18 +3,9 @@ package ink.pmc.framework.bridge.server
 import ink.pmc.framework.bridge.Bridge
 import ink.pmc.framework.bridge.player.BridgePlayer
 
-class BridgeGroupImpl(override val id: String) : BridgeGroup {
+data class BridgeGroupImpl(override val id: String) : BridgeGroup {
     override val players: Collection<BridgePlayer>
         get() = Bridge.players.filter { it.group == this }
     override val servers: Collection<BridgeServer>
         get() = Bridge.servers.filter { it.group == this }
-
-    override fun equals(other: Any?): Boolean {
-        if (other !is BridgeGroup) return false
-        return other.id == id
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
 }
