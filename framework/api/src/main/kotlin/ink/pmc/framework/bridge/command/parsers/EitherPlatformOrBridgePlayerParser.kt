500385c20626a53b71320969686703075fe125aa
nostalfinals <yuu8583@proton.me>
2024-11-11 21:41:22 +0800

    feat(framework): 添加 EitherPlatformOrBridgePlayerParser
    - 实现了一个新的参数解析器，可以解析平台玩家或 Bridge 玩家

---
+++ framework/api/src/main/kotlin/ink/pmc/framework/bridge/command/parsers/EitherPlatformOrBridgePlayerParser.kt
@@ -0,0 +1,66 @@
+package ink.pmc.framework.bridge.command.parsers
+
+import ink.pmc.framework.bridge.player.BridgePlayer
+import ink.pmc.framework.utils.concurrent.submitAsync
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.future.await
+import org.incendo.cloud.context.CommandContext
+import org.incendo.cloud.context.CommandInput
+import org.incendo.cloud.parser.ArgumentParseResult
+import org.incendo.cloud.parser.ArgumentParser
+import org.incendo.cloud.parser.ArgumentParser.FutureArgumentParser
+import org.incendo.cloud.parser.ParserDescriptor
+import org.incendo.cloud.parser.standard.StringParser
+import org.incendo.cloud.suggestion.Suggestion
+import org.incendo.cloud.suggestion.SuggestionProvider
+import org.incendo.cloud.type.Either
+import java.util.concurrent.CompletableFuture
+import kotlin.jvm.optionals.getOrNull
+
+fun <P : Any> eitherPlatformOrBridgePlayerParser(platformParser: ArgumentParser<P, BridgePlayer>) {
+    EitherPlatformOrBridgePlayerParser(platformParser)
+}
+
+fun <P : Any> eitherPlatformOrBridgePlayerParser(platformParser: ParserDescriptor<P, BridgePlayer>) {
+    eitherPlatformOrBridgePlayerParser(platformParser.parser())
+}
+
+class EitherPlatformOrBridgePlayerParser<C : Any, P : Any>(
+    private val platformParser: ArgumentParser<C, P>
+) : FutureArgumentParser<C, Either<P, BridgePlayer>>,
+    SuggestionProvider<C> {
+    private val stringParser = StringParser.quotedStringParser<C>().parser()
+    private val bridgePlayerParser = bridgePlayerParser<C>().parser()
+
+    override fun parseFuture(
+        commandContext: CommandContext<C>,
+        commandInput: CommandInput
+    ): CompletableFuture<ArgumentParseResult<Either<P, BridgePlayer>>> =
+        submitAsync<ArgumentParseResult<Either<P, BridgePlayer>>> {
+            val name = stringParser.parseFuture(commandContext, commandInput).await().parsedValue().get()
+            val platform = platformParser.parseFuture(commandContext, commandInput).await().parsedValue().getOrNull()
+            val bridge = bridgePlayerParser.parseFuture(commandContext, commandInput).await().parsedValue().getOrNull()
+            val either = if (platform != null) {
+                Either.ofPrimary(platform)
+            } else if (bridge != null) {
+                Either.ofFallback(bridge)
+            } else {
+                throw BridgePlayerNotFoundException(name)
+            }
+            ArgumentParseResult.success(either)
+        }.asCompletableFuture()
+
+    override fun suggestionsFuture(
+        context: CommandContext<C>,
+        input: CommandInput
+    ): CompletableFuture<List<Suggestion>> = submitAsync<List<Suggestion>> {
+        val platform = platformParser.suggestionProvider().suggestionsFuture(context, input).await()
+        val bridge = bridgePlayerParser.suggestionProvider().suggestionsFuture(context, input).await()
+        val result = (platform + bridge).distinctBy { it.suggestion() }
+        result.toList()
+    }.asCompletableFuture()
+
+    override fun suggestionProvider(): SuggestionProvider<C> {
+        return this
+    }
+}
