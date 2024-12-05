f54d134b07a69b57c27dccdd9761dcc47804fa0e
nostalfinals <yuu8583@proton.me>
2024-12-05 11:35:18 +0800

    refactor: 默认关闭 Dynamic View Distance

--- hypervisor/paper/src/main/kotlin/ink/pmc/hypervisor/OptionDescriptors.kt
+++ hypervisor/paper/src/main/kotlin/ink/pmc/hypervisor/OptionDescriptors.kt
@@ -6,5 +6,5 @@ import ink.pmc.framework.options.dsl.descriptor
 val DYNAMIC_VIEW_DISTANCE = descriptor<Boolean> {
     key = "hypervisor.dynamic_view_distance"
     type = EntryValueType.BOOLEAN
-    defaultValue = true
+    defaultValue = false
 }
