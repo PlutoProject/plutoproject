05bfaf741e0712d6fe5da1444c8f68cb0329517f
nostalfinals <yuu8583@proton.me>
2024-12-06 15:35:35 +0800

    fix: 修复 HomeListScreenModel 排序不正确

--- essentials/paper/src/main/kotlin/ink/pmc/essentials/screens/home/HomeListScreenModel.kt
+++ essentials/paper/src/main/kotlin/ink/pmc/essentials/screens/home/HomeListScreenModel.kt
@@ -11,9 +11,12 @@ private const val PAGE_SIZE = 28
 class HomeListScreenModel(private val viewing: OfflinePlayer) : ListMenuModel<Home>() {
     override suspend fun fetchPageContents(): List<Home> {
         val homes = HomeManager.list(viewing).toList()
-            .sortedByDescending { it.createdAt }
             .sortedBy {
-                if (it.isStarred || it.isPreferred) 0 else 1
+                when {
+                    it.isPreferred -> 0
+                    it.isStarred -> 1
+                    else -> 2
+                }
             }
         pageCount = ceil(homes.size.toDouble() / PAGE_SIZE).toInt()
         return homes.drop(page * PAGE_SIZE).take(PAGE_SIZE)
