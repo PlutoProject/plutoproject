23c67597ae53eb578174ad8660e79affbaa1b37f
nostalfinals <yuu8583@proton.me>
2024-12-16 20:39:37 +0800

    DSLs

---
+++ menu/api/src/main/kotlin/ink/pmc/menu/api/dsl/ButtonDescriptorDsl.kt
@@ -0,0 +1,19 @@
+package ink.pmc.menu.api.dsl
+
+import ink.pmc.framework.utils.structure.Builder
+import ink.pmc.menu.api.descriptor.ButtonDescriptor
+import ink.pmc.menu.api.factory.ButtonDescriptorFactory
+
+class ButtonDescriptorDsl : Builder<ButtonDescriptor> {
+    var id: String? = null
+
+    override fun build(): ButtonDescriptor {
+        return ButtonDescriptorFactory.create(
+            id = id ?: error("Id not set")
+        )
+    }
+}
+
+inline fun buttonDescriptor(block: ButtonDescriptorDsl.() -> Unit): ButtonDescriptor {
+    return ButtonDescriptorDsl().apply(block).build()
+}
