e3ecef3d3ba13e04a24aa826a7059cd760beee71
nostalfinals <yuu8583@proton.me>
2024-12-16 17:08:28 +0800

    加载

--- menu/paper/src/main/kotlin/ink/pmc/menu/PaperPlugin.kt
+++ menu/paper/src/main/kotlin/ink/pmc/menu/PaperPlugin.kt
@@ -1,19 +1,36 @@
 package ink.pmc.menu
 
 import com.github.shynixn.mccoroutine.bukkit.SuspendingJavaPlugin
+import com.sksamuel.hoplite.PropertySource
 import ink.pmc.framework.utils.command.annotationParser
 import ink.pmc.framework.utils.command.commandManager
+import ink.pmc.framework.utils.config.preconfiguredConfigLoaderBuilder
+import ink.pmc.framework.utils.inject.startKoinIfNotPresent
+import ink.pmc.framework.utils.storage.saveResourceIfNotExisted
 import ink.pmc.menu.commands.MenuCommand
 import net.milkbowl.vault.economy.Economy
 import org.bukkit.plugin.java.JavaPlugin
+import org.koin.dsl.module
 
 lateinit var plugin: JavaPlugin
 lateinit var economy: Economy
 
 @Suppress("UNUSED")
 class PaperPlugin : SuspendingJavaPlugin() {
+    private val bukkitModule = module {
+        single<MenuConfig> {
+            preconfiguredConfigLoaderBuilder()
+                .addPropertySource(PropertySource.file(saveResourceIfNotExisted("config.conf")))
+                .build()
+                .loadConfigOrThrow()
+        }
+    }
+
     override suspend fun onEnableAsync() {
         plugin = this
+        startKoinIfNotPresent {
+            modules(bukkitModule)
+        }
         commandManager().annotationParser().apply {
             parse(MenuCommand)
         }
